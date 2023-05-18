8fa8b68325e21d486e8228f2c0417d42019dfa0b
GianMarco <gpaldino@ulb.ac.be>
2023-05-18 13:53:40 +0200

    trying html

--- conf.py
+++ conf.py
@@ -2,17 +2,16 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 import os
 import sys
-path = os.path.abspath('../../')
-print(path)
+path = os.path.abspath('.')
 sys.path.insert(0, path)
+
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'D2C'
+project = 'DC2PY'
 copyright = '2023, Gian Marco Paldino, Gianluca Bontempi'
 author = 'Gian Marco Paldino, Gianluca Bontempi'
 
@@ -21,16 +20,13 @@ author = 'Gian Marco Paldino, Gianluca Bontempi'
 
 extensions = ['sphinx.ext.autodoc']
 
-templates_path = ['_templates']
-exclude_patterns = ['../temp/*']
+templates_path = ['templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_logo = 'd2c.png'
-
-html_dir = '..'
-html_static_path = ['_static']
-master_doc = 'index'
+html_static_path = ['./static']
