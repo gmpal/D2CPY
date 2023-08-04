62b8350a522b7203278914ee5d7e3a4cc2dd59c3
GianMarco <gpaldino@ulb.ac.be>
2023-08-04 19:42:30 +0200

    generateTS

---
+++ generation/generateTS.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append("..")
+import numpy as np
+from d2c.simulatedTimeSeries import SimulatedTimeSeries
+import pickle
+
+if __name__ == "__main__":
+    n_series = 10000  # You can change this as needed
+    n_observations = 150
+    n_variables = 3
+    generator = SimulatedTimeSeries(n_series,  n_observations, n_variables, n_jobs=30)
+    generator.generate()
+
+    observations = generator.get_observations()
+    dags = generator.get_dags()
+    updated_dags = generator.get_updated_dags()
+
+    #pickle everything
+    with open('observations.pkl', 'wb') as f:
+        pickle.dump(observations, f)
+
+    with open('dags.pkl', 'wb') as f:
+        pickle.dump(dags, f)
+
+    with open('updated_dags.pkl', 'wb') as f:
+        pickle.dump(updated_dags, f)
+
+
+
