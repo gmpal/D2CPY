edd8f8ac4fb54b7945615d67df6b3f317cff390d
GianMarco <gpaldino@ulb.ac.be>
2023-11-19 23:57:20 +0100

    same for .py

--- execute.py
+++ execute.py
@@ -13,7 +13,7 @@ command = [
     'python', 'generateTS.py',
     '--n_series', '10',
     '--n_observations', '150',
-    '--name', 'deleteme',
+    '--name', 'mix',
     '--n_jobs', '10',
     '--function_types'
 ] + function_types_list
@@ -21,10 +21,42 @@ command = [
 subprocess.run(command)
 
 # # Run the second Python script with specified arguments
-subprocess.run(['python', 'd2c_past_gen.py', '--name=deleteme', '--maxlags=3', '--n_jobs=10'])
+subprocess.run(['python', 'd2c_past_gen.py', '--name=mix', '--maxlags=3', '--n_jobs=10'])
 
 # Navigate to the 'competitors' directory from the project root
 os.chdir('../competitors')
 
 # Run the third Python script with specified arguments
-subprocess.run(['python', 'test.py', '--name=deleteme', '--maxlags=3', '--n_jobs=10'])
+subprocess.run(['python', 'test.py', '--name=mix', '--maxlags=3', '--n_jobs=10'])
+
+##############################################################################################################
+##############################################################################################################
+##############################################################################################################
+
+for function in function_types_list:
+    os.chdir('../generation')
+
+    # # Run the first Python script with specified arguments
+    # Example function_types list
+    
+
+    # Prepare the command with function types
+    command = [
+        'python', 'generateTS.py',
+        '--n_series', '10',
+        '--n_observations', '150',
+        '--name', 'only_'+function,
+        '--n_jobs', '10',
+        '--function_types'
+    ] + [function]
+
+    subprocess.run(command)
+
+    # # Run the second Python script with specified arguments
+    subprocess.run(['python', 'd2c_past_gen.py', '--name=only_'+function, '--maxlags=3', '--n_jobs=10'])
+
+    # Navigate to the 'competitors' directory from the project root
+    os.chdir('../competitors')
+
+    # Run the third Python script with specified arguments
+    subprocess.run(['python', 'test.py', '--name=only_'+function, '--maxlags=3', '--n_jobs=10'])
